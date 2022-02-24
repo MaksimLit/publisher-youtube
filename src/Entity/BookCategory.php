2ed9fc669822f1e16822d71f82c754c9632d440f
Maxim Litvyakov <maxim.litvyakov@sibers.com>
2022-02-24 15:29:23 +0700

    Added tests

--- src/Entity/BookCategory.php
+++ src/Entity/BookCategory.php
@@ -25,6 +25,13 @@ class BookCategory
         return $this->id;
     }
 
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getTitle(): ?string
     {
         return $this->title;
